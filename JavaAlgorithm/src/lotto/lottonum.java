da994404105f50e3d35439fbe193b38f4c377e82
it <imleeseong@gmail.com>
2018-01-12 10:49:27 +0900

    lotto algorithm

--- JavaAlgorithm/src/lotto/lottonum.java
+++ JavaAlgorithm/src/lotto/lottonum.java
@@ -5,14 +5,18 @@ import java.util.*;
 public class lottonum {
 	private static Scanner sc;
 	// 버블정렬 메소드
-	static boolean bubbleSort(int arr[]) {
+	static boolean bubbleSort(int arr[],int bonus) {
 		int temp = 0;
-		for (int j = 0; j < arr.length - 2; j++) {
-			for (int i = 0; i < arr.length - 2; i++) {
+		for (int j = 0; j < arr.length - bonus; j++) {
+			for (int i = 0; i < arr.length - bonus; i++) {
 				if (arr[i] > arr[i + 1]) {
 					temp = arr[i];
 					arr[i] = arr[i + 1];
 					arr[i + 1] = temp;
+				}
+				for(int k=0;k<arr.length-2;k++) {
+					if(arr[k] == arr[arr.length-1])
+						return true;
 				}
 				if (arr[i] == arr[i + 1]) {
 					return true;
@@ -22,9 +26,10 @@ public class lottonum {
 		return false;
 	}
 	// 배열의 값을 출력하는 메소드
-	static void numPrint(int arr[]) {
+	static void numPrint(int arr[],int bonus) {
 		for (int i = 0; i < arr.length; i++) {
-			System.out.print(arr[i] + " ");
+			if(i!=bonus)	System.out.print(arr[i] + " ");
+			else	System.out.print(arr[i] + " ");
 		}
 		System.out.println();
 	}
@@ -37,19 +42,24 @@ public class lottonum {
 					count++;
 			}
 		}
-		if(count==6 && arr[6]==arr2[6]) count++; 
+		if(count==5) 
+			for (int j = 0; j < arr2.length-1; j++) {
+				if (arr[5] != arr2[j])
+					count--;
+			}
 		return count;
 	}
 	
 	public static void main(String[] args) {
-		int lotto[] = new int[7]; // 로또 넘버를 저장하는 배열
-		int insertNum[] = new int[7]; // 구입하는 로또번호(직접 입력/자동생성)
+		int lotto[] = new int[6]; // 로또 넘버를 저장하는 배열
+		int insertNum[] = new int[6]; // 구입하는 로또번호(직접 입력/자동생성)
 		boolean reNum = false; // 로또 당첨 번호의 중복을 체크하는 변수
 		boolean reInsert = false; // 직접 입력할 때 중복을 체크하는 변수
 		int select = 0; // 직접 입력할 것인지, 랜덤으로 번호를 추출할 것인지를 선택하는 변수
 		int compareNum = 0; // 로또 당첨번호와 나의 번호를 비교하여 같은 번호가 몇개 있는지를 반환한다.
 		int money = 0; // 당첨금
 		int price[] = {0,0,5000,50000,5000000,500000000,500000000};
+		String rank[] = {"꽝","꽝","5등","4등","3등","2등","1등"};
 		Random lottoNum = new Random();
 		while (true) {
 			System.out.println("직접입력 : 1, 자동생성 : 2");
@@ -66,10 +76,9 @@ public class lottonum {
 		case 1: // 직접입력하는 경우
 			do {
 				int cnt = 0;
-				while (cnt < 7) {
+				while (cnt < insertNum.length) {
 					try {
-						if(cnt==6) System.out.println("1~45까지 중 선택. (보너스)");
-						else System.out.println("1~45까지 중 선택. (" + (cnt + 1) + "번째)");
+						System.out.println("1~45까지 중 선택. (" + (cnt + 1) + "번째)");
 						sc = new Scanner(System.in);
 						insertNum[cnt] = sc.nextInt();
 						if (insertNum[cnt] < 1 || insertNum[cnt] > 45) {
@@ -86,14 +95,14 @@ public class lottonum {
 						System.out.println(e.getMessage());
 					}
 				}
-				reInsert = bubbleSort(insertNum);
+				reInsert = bubbleSort(insertNum,1);
 				cnt = 0;
 				if (reInsert == true) {
 					System.out.println("중복 값 존재. 재입력.\n");
 				}
 			} while (reInsert);
 			System.out.print("\nMy Number : ");
-			numPrint(insertNum); // 입력한 로또 번호 출력
+			numPrint(insertNum,-1); // 입력한 로또 번호 출력
 			break;
 		case 2: // 자동으로 번호 생성하는 경우
 			for (int i = 0; i < insertNum.length; i++) {
@@ -112,9 +121,9 @@ public class lottonum {
 					}
 				}
 			}
-			bubbleSort(insertNum);
+			bubbleSort(insertNum,1);
 			System.out.print("\nMy Number : ");
-			numPrint(insertNum); // 자동 추출한 로또 번호 출력
+			numPrint(insertNum,-1); // 자동 추출한 로또 번호 출력
 			break;
 		}
 		/*
@@ -126,13 +135,13 @@ public class lottonum {
 			for (int i = 0; i < lotto.length; i++) {
 				lotto[i] = lottoNum.nextInt(45) + 1; // 0~44까지 난수를 발생시켜서 1을 더함
 			}
-			reNum = bubbleSort(lotto); // 정렬을 하다 중복값이 발생하면 true값을 반환
+			reNum = bubbleSort(lotto,2); // 정렬을 하다 중복값이 발생하면 true값을 반환
 		} while (reNum);
 		compareNum = compare(lotto, insertNum); // 같은 번호가 몇개인지 비교
 		money = price[compareNum];
 		// 로또번호 출력
 		System.out.print("Lotto Number : ");
-		numPrint(lotto);
-		System.out.println("\n일치하는 번호는 총 " + compareNum + "개 이고 당첨금액은 " + money + "원 입니다.");
+		numPrint(lotto,lotto.length-1);
+		System.out.println("\n" + rank[compareNum] + ": " + money + "원");
 	}
 }
